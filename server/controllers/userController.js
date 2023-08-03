2deab7759fe169534c32e87dc6d4ed0191a62932
MosesK <mosesziongo@gmail.com>
2023-08-03 23:09:00 +0200

    generating of token with jsonwebtoken

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,6 +3,12 @@ const bcrypt = require("bcrypt");
 const validator = require("validator");
 const jwt = require("jsonwebtoken");
 
+const createToken = (_id) => {
+  const jwtkey = process.env.JWT_SECRET_KEY;
+
+  return jwt.sign({ _id }, jwtkey, { expiresIn: "3d" });
+};
+
 const registerUser = async (req, res) => {
   const { name, email, password } = req.body;
 
@@ -19,6 +25,15 @@ const registerUser = async (req, res) => {
 
   if (!validator.isStrongPassword(password))
     return res.status(400).json("Password must be a strong password...");
+
+  user = new userModel({ name, email, password });
+
+  const salt = await bcrypt.genSalt(10);
+  user.password = bcrypt.hash(user.password, salt);
+
+  await user.save();
+
+  const token = createToken(user._id);
 };
 
 module.exports = { registerUser };
