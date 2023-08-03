d537ea9c6e256913eeddd6deb7a07450f5e31850
MosesK <mosesziongo@gmail.com>
2023-08-03 23:47:42 +0200

    login implementation

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -12,28 +12,52 @@ const createToken = (_id) => {
 const registerUser = async (req, res) => {
   const { name, email, password } = req.body;
 
-  let user = await userModel.findOne({ email });
+  try {
+    let user = await userModel.findOne({ email });
 
-  if (user)
-    return res.status(400).json("User with the given email already exist...");
+    if (user)
+      return res.status(400).json("User with the given email already exist...");
 
-  if (!name || !email || !password)
-    return res.status(400).json("All fields are required...");
+    if (!name || !email || !password)
+      return res.status(400).json("All fields are required...");
 
-  if (!validator.isEmail(email))
-    return res.status(400).json("Email must be a valid email...");
+    if (!validator.isEmail(email))
+      return res.status(400).json("Email must be a valid email...");
 
-  if (!validator.isStrongPassword(password))
-    return res.status(400).json("Password must be a strong password...");
+    if (!validator.isStrongPassword(password))
+      return res.status(400).json("Password must be a strong password...");
 
-  user = new userModel({ name, email, password });
+    user = new userModel({ name, email, password });
 
-  const salt = await bcrypt.genSalt(10);
-  user.password = bcrypt.hash(user.password, salt);
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
 
-  await user.save();
+    await user.save();
 
-  const token = createToken(user._id);
+    const token = createToken(user._id);
+
+    res.status(200).json({ _id: user._id, name, email, token });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
 };
 
-module.exports = { registerUser };
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    let user = await userModel.findOne({ email });
+    if (!user) return res.status(400).json("Invalid email or password...");
+
+    const isValidPassword = await bcrypt.compare(password, user.password);
+
+    if (!isValidPassword)
+      return res.status(400).json("Invalid email or password...");
+
+      const token = createToken(user._id);
+      res.status(200).json({_id: user._id, name : user.name, email, token})
+  } catch (error) {}
+};
+
+module.exports = { registerUser, loginUser };
