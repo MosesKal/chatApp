ce24cf3a444f156713c9b3372c04d6ae7925787f
MosesK <mosesziongo@gmail.com>
2023-08-04 00:23:43 +0200

    finalisation de lAPI

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -71,4 +71,17 @@ const findUser = async (req, res) => {
   }
 };
 
-module.exports = { registerUser, loginUser, findUser };
+const getUser = async (req, res) => {
+
+    try {
+      const users = await userModel.find();
+
+      res.status(200).json(users);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json(error);
+    }
+  };
+
+
+module.exports = { registerUser, loginUser, findUser, getUser };
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,10 +1,16 @@
 const express = require("express");
-const { registerUser,loginUser,findUser } = require("../controllers/userController");
+const {
+  registerUser,
+  loginUser,
+  findUser,
+  getUser,
+} = require("../controllers/userController");
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/find/:userId", findUser);
+router.get("/", getUser);
 
 module.exports = router;
