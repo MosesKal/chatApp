6200e893ade59fc03602597a57e661d1632b0564
MosesK <mosesziongo@gmail.com>
2023-08-13 17:08:36 +0200

    get messages

---
+++ client/src/components/chat/ChatBox.jsx
@@ -0,0 +1,21 @@
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
+import { ChatContext } from "../../context/ChatContext";
+import { useFetchRecipientUser } from "../../hooks/useFetchRecipient";
+
+const ChatBox = () => {
+  const { user } = useContext(AuthContext);
+  const { currentChat, messages, isMessagesLoading } = useContext(ChatContext);
+  const { recipientUser } = useFetchRecipientUser(currentChat, user);
+
+  if (!recipientUser)
+    return (
+      <p style={{ textAlign: "center", width: "100%" }}>
+        No conversation selected yet...
+      </p>
+    );
+
+  return <>ChatBox</>;
+};
+
+export default ChatBox;
--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -4,6 +4,7 @@ import { Container, Stack } from "react-bootstrap";
 import UserChat from "../components/chat/UserChat";
 import { AuthContext } from "../context/AuthContext";
 import PotentialChat from "../components/chat/PotentialChat";
+import ChatBox from "../components/chat/ChatBox";
 
 const Chat = () => {
   const { user } = useContext(AuthContext);
@@ -12,18 +13,18 @@ const Chat = () => {
 
   return (
     <Container>
-      <PotentialChat/>
+      <PotentialChat />
       {userChats?.length < 1 ? null : (
         <Stack direction="horizontal" gap={4} className="align-items-start">
           <Stack className="messages-box flex-grow-0 pe-3" gap={3}>
             {isUserChatsLoading && <p>Loading chats...</p>}
             {userChats?.map((chat, index) => (
-              <div key={index} onClick={()=>updateCurrentChat(chat)}>
+              <div key={index} onClick={() => updateCurrentChat(chat)}>
                 <UserChat chat={chat} user={user} />
               </div>
             ))}
           </Stack>
-          <p>ChatBox</p>
+          <ChatBox />
         </Stack>
       )}
     </Container>
